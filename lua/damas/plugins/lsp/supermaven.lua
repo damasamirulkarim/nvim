0db38793652175893f03eaebffbcd867c215444b
Damas Amirul Karim <damasamirulkarim@gmail.com>
2025-09-27 04:19:34 +0700

    install supermaven

---
+++ lua/damas/plugins/lsp/supermaven.lua
@@ -0,0 +1,15 @@
+return {
+	"supermaven-inc/supermaven-nvim",
+	event = "InsertEnter",
+	config = function()
+		require("supermaven-nvim").setup({
+			keymaps = {
+				accept_suggestion = "<Tab>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C-j>",
+			},
+			disable_inline_completion = false, -- keep inline ghost text
+			disable_keymaps = false,
+		})
+	end,
+}

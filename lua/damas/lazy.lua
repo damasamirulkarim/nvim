355d0a1550e63c2a93e8bef076763c22df18ff48
Damas Amirul Karim <damasamirulkarim@gmail.com>
2025-09-27 02:44:36 +0700

    fix lazy git

--- lua/damas/lazy.lua
+++ lua/damas/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("damas.plugins", { import = "damas.plugins.lsp" },{
+require("lazy").setup({ { import = "damas.plugins" }, { import = "damas.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
